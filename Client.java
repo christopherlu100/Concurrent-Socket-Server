1ee5bb0c0e5ae3fdca355afa02151b3e522c72fb
christopherlu100 <christopherlu100@gmail.com>
2025-05-13 01:13:39 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,13 +2,15 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+// Threadhandler class extends Thread to handle multiple client requests concurrently
 class ThreadHandler extends Thread {
-    private String address;
-    private int port;
-    private String input;
-    private int counter;
-    private long time;
+    private String address;    // Server address
+    private int port;          // Server port number
+    private String input;      // Command input from client
+    private int counter;       // Client request counter
+    private long time;         // Time taken for the request
 
+    // COntrustor for initializing thread parameters
     public ThreadHandler(String address, int port, String input, int counter) {
         this.address = address;
         this.port = port;
@@ -16,32 +18,37 @@ class ThreadHandler extends Thread {
         this.counter = counter;
     }
 
+    // Getter to retrieve request time
     public long getTime() {
         return time;
     }
 
+    // Define thread behavior
     public void run() {
         try {
-            long starttime = System.nanoTime();
+            long starttime = System.nanoTime(); // Start tracking grequest time
 
+            // Establish a socket connection to the server
             Socket socket = new Socket(address, port);
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
+            // Send the user command to the server
             out.println(input);
             String line;
-            //String output = in.readLine();
 
-
-
-            //System.out.println(output);
+            // Read the server response and print it
             while ((line = in.readLine()) != null) {
                 System.out.println(line);
             }
+
+            // Calc the time taken for requests
             long endtime = System.nanoTime();
             time = (endtime - starttime)/1000000;
             System.out.println("Turn-around time for client request number " + counter + ": " + (endtime - starttime) / 1000000 + "ms\n");
 
+
+            // Close socket connection
             socket.close();
         } catch (IOException e) {
             System.out.println(e);
@@ -55,6 +62,7 @@ public class Client {
 
         Scanner sc = new Scanner(System.in);
 
+        // User input for network address and port
         System.out.println("Enter Network Address:");
         String address = sc.nextLine();
 
@@ -62,8 +70,8 @@ public class Client {
         int port = sc.nextInt();
         sc.nextLine();
 
+        // Infintite loop to send client requests
         while (true) {
-
             System.out.println("Please Enter Number Corresponding to Desired Command:");
             System.out.println("1 - Date and Time");
             System.out.println("2 - Uptime");
@@ -73,14 +81,17 @@ public class Client {
             System.out.println("6 - Running Process");
             System.out.println("7 - Exit Program");
 
+            // User command input
             String input = sc.nextLine();
 
+            // Exit if user selects 7
             if (input.equals("7")) {
                 System.out.println("Exiting Program");
                 System.exit(0);
                 break;
             }
 
+            // User input for number of concurrent requesets
             System.out.println("Please Enter Number of Request");
             System.out.println("Options: 1, 5, 10, 15, 20, 25, 100");
             int requests = sc.nextInt();
@@ -97,12 +108,12 @@ public class Client {
                     System.out.println("Invalid Number of Request");
                     continue;
             }
-            sc.nextLine();
+            sc.nextLine(); // Clears buffer
 
+            // Creating an array of threds based on number of requests
             ThreadHandler[] threads = new ThreadHandler[requests];
 
-
-
+            // Initializing and starting each client requests thread
             for (int i = 0; i < requests; i++) {
                 threads[i] = new ThreadHandler(address, port, input, i + 1);
                 threads[i].start();
@@ -110,6 +121,7 @@ public class Client {
 
             long totalTime = 0;
 
+            // Wait for each thread to complete and calculate total time
             for (int i = 0; i < requests; i++) {
                 try {
                     threads[i].join();
@@ -120,13 +132,13 @@ public class Client {
 
             }
 
-
-
+            // Display total and average turn around time for all requests
             System.out.println("Total turn-around time for client requests: " + totalTime +"ms");
             System.out.println("Average turn-around time for client requsets: " + (totalTime/requests) +"ms\n");
 
         }
-        sc.close();
+        
+        sc.close(); // Close scanner
     }
 }
 
